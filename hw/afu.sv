8a8b3775776d86736b178fafed9d0c76f6bcf790
snowtothemax <mkjohnson9@wisc.edu>
2021-09-09 17:42:55 -0500

    changed a little something

--- hw/afu.sv
+++ hw/afu.sv
@@ -77,7 +77,7 @@ module afu
    
    // instantiate a FIFO
    fifo fifo_1(.clk(clk), .rst_n(rst_n), .en(en_fifo),
-	        .d(d_fifo), .q(q_fifo));
+	        .d(rx.c0.data[63:0]), .q(q_fifo));
 
    // =============================================================//   
    // MMIO write code
